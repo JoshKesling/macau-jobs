04bc96d1c9d9ab3609689f4d755d74a3e34e6698
Josh Kesling <10089440+JoshKesling@users.noreply.github.com>
2018-02-16 17:04:45 +0800

    Edit ENV VARS

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,13 +95,13 @@ Rails.application.configure do
 
   #paperclip S3 
   config.paperclip_defaults = {
-    storage: :s3,
-    s3_region: ENV["AWS_S3_REGION"],
-    s3_credentials: {
-      s3_host_name: ENV["AWS_S3_HOST_NAME"],
-      bucket: ENV["AWS_S3_BUCKET"],
-      access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-      secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
-      }
-  } 
+  storage: :s3,
+  s3_host_name: "s3-us-west-2.amazonaws.com",
+  s3_region: ENV['AWS_REGION'],
+  s3_credentials: {
+    bucket: ENV['AWS_BUCKET'],
+    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  }
+}
 end
